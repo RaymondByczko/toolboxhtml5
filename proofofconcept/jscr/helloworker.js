abc24d71f70d5ba43d369b3ec4b0c16579a3be9a
Raymond Byczko <raymondbyczko@att.net>
2013-05-05 07:40:58 -0400

    self-RByczko; 2013-05-05 May 5; Delayed commit of modified work.

--- proofofconcept/jscr/helloworker.js
+++ proofofconcept/jscr/helloworker.js
@@ -5,6 +5,10 @@ onmessage = function(event)
 	var n = parseInt(event.data);
 	allSentNumbers.push(n);
 	var five = 5;
+	self.setTimeout(function(){},4000);
 	postMessage("allSentNumbers(size)=" + five.toString());
+	self.setTimeout(function(){},4000);
+	postMessage("endthread");
+	self.close();
 }
 
