da776bc6c2db5e1f76c2997dd9a7ce0695cf91f2
Raymond Byczko <raymondbyczko@att.net>
2013-05-07 11:25:28 -0400

    self-RByczko; 2013-05-07; Slideshow demo (adjust path for subdomain).

--- slideshow/circles.php
+++ slideshow/circles.php
@@ -16,8 +16,8 @@ div.cscene {
 border: 5px solid #feb515;
 -webkit-border-radius: 3px;
 -moz-border-radius: 3px;
--webkit-border-image: url(/slideshow/border.jpg) 5 5 stretch;
--moz-border-image: url(/slideshow/border.jpg) 5 5 stretch;
+-webkit-border-image: url(/border.jpg) 5 5 stretch;
+-moz-border-image: url(/border.jpg) 5 5 stretch;
 }
 #circle1 {
 	-webkit-animation: track-scene1;
@@ -59,14 +59,14 @@ margin-left: 120px;
 <layer id="layer1">
 <div id="clipedge" style="position: absolute; top: 0px; left: 0px; height: 100px; width: 100px">
 <div id="circle1" style="position:absolute; height: 10px; width: 10px">
-<img src="/slideshow/circle.jpg"/>
+<img src="/circle.jpg"/>
 </div>
 </div>
 </layer>
 <layer id="layer2">
 <div id="clipedge" style="position: absolute; top: 0px; left: 0px; height: 100px; width: 100px">
 <div id="circle2" style="position:absolute; height: 10px; width: 10px">
-<img src="/slideshow/bcircle.jpg"/>
+<img src="/bcircle.jpg"/>
 </div>
 </div>
 </layer>
--- slideshow/fish.php
+++ slideshow/fish.php
@@ -16,8 +16,8 @@ div.cscene {
 border: 5px solid #feb515;
 -webkit-border-radius: 3px;
 -moz-border-radius: 3px;
--webkit-border-image: url(/slideshow/border.jpg) 5 5 stretch;
--moz-border-image: url(/slideshow/border.jpg) 5 5 stretch;
+-webkit-border-image: url(/border.jpg) 5 5 stretch;
+-moz-border-image: url(/border.jpg) 5 5 stretch;
 }
 #fish {
 	-webkit-animation: track-scene-fish;
@@ -93,21 +93,21 @@ margin-left: 80px;
 <layer id="layer3">
 <div id="clipedge" style="position: absolute; top: 0px; left: 0px; height: 100px; width: 100px">
 <div id="bubbleX3" style="position:absolute; height: 20px; width: 20px">
-<img src="/slideshow/bubblesX3.png"/>
+<img src="/bubblesX3.png"/>
 </div>
 </div>
 </layer>
 <layer id="layer1">
 <div id="clipedge" style="position: absolute; top: 0px; left: 0px; height: 100px; width: 100px">
 <div id="fish" style="position:absolute; height: 10px; width: 10px">
-<img src="/slideshow/tunahead2.png"/>
+<img src="/tunahead2.png"/>
 </div>
 </div>
 </layer>
 <layer id="layer2">
 <div id="clipedge" style="position: absolute; top: 0px; left: 0px; height: 100px; width: 100px">
 <div id="bubble" style="position:absolute; height: 10px; width: 10px">
-<img src="/slideshow/bcircle.jpg"/>
+<img src="/bcircle.jpg"/>
 </div>
 </div>
 </layer>
